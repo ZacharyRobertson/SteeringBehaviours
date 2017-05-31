dc03162609a8f00353f1f00d56f3d6b9c45136c0
ZacharyRobertson <ztrsec@hotmail.com>
2017-05-31 12:25:08 +1000

    Uplaoding XML scripts

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public float spawnRate = 1f;
+
+    private float spawnTimer = 0f;
+    public List<GameObject> objects = new List<GameObject>();
+
+
+    void OnDrawGizmos()
+    {
+        // Draw a Cube to indicate where the box is that we're spawning objects
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set spawnTimer to spawnTimer + deltaTime
+        spawnTimer = spawnTimer + Time.deltaTime;
+        // If spawnTimer > spawnRate 
+        if (spawnTimer > spawnRate)
+        {
+            // Set randomPoint to GenerateRandomPoint
+            Vector3 randomPoint = GenerateRandomPoint();
+            // Call Spawn and pass randomPoint, Quaternion identity
+            Spawn(randomPoint, Quaternion.identity);
+            // Set spawnTimer to zero
+            spawnTimer = 0;
+        }
+    }
+    // Generates a random point within transform's local scale
+    Vector3 GenerateRandomPoint()
+    {
+        // Set halfScale to half of the transform's scale
+        Vector3 halfScale = transform.localScale * 0.5f;
+        // Set Random point vector to zero
+        Vector3 randomPoint = Vector3.zero;
+        // Set random point x,y & z to RandomRange between -halfScale to halfScale(Can do individually)
+        randomPoint.x = Random.Range(-halfScale.x, halfScale.x);
+        randomPoint.y = Random.Range(-halfScale.y, halfScale.y);
+        randomPoint.z = Random.Range(-halfScale.z, halfScale.z);
+        // Return the randomPoint
+        return randomPoint;
+    }
+    // Spawn the prefab at a given position and with rotation
+    public void Spawn(Vector3 position, Quaternion rotation)
+    {
+        // Set clone to new instance of prefab
+        GameObject clone = Instantiate(prefab);
+        // add clone to objects list
+        objects.Add(clone);
+        // Set clone's position to spawner position + position
+        clone.transform.position = transform.position + position;
+        // Set clone's rotation to rotation
+        clone.transform.rotation = rotation;
+    }
+}
---
+++ Assets/Scripts/SpawnerXML.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using UnityEngine;
+
+[RequireComponent(typeof(Spawner))]
+public class SpawnerXML : MonoBehaviour
+{
+    // Stores individual data associated with each spawned object
+    public class SpawnerData
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+    }
+    [XmlRoot]
+    public class XMLContainer
+    {
+       [XmlArray]
+       public SpawnerData[] spawners;
+    }
+
+    public string fileName = "DefaultFileName";
+
+    private Spawner spawner;
+    private string fullPath;
+   
+    // data container for XML
+    private XMLContainer data;
+
+    // Saves XMLContainer instance to a file as XML format
+    void SaveToPath(string path)
+    {
+        // Create a serializer of type XMLContainer
+        XmlSerializer serializer = new XmlSerializer(typeof(XMLContainer));
+        // Open a file stream at path using Create file mode
+        using (FileStream stream = new FileStream(path, FileMode.Create))
+        {
+            // Serialize stream to data
+            serializer.Serialize(stream, data);
+        }
+    }
+
+    // Loads XML container from path (only run if the file exists)
+    XMLContainer Load(string path)
+    {
+        //Create a serializer of type XML Container
+        XmlSerializer serializer = new XmlSerializer(typeof(XMLContainer));
+        // Open a file stream at path using Open file mode
+        using (FileStream stream = new FileStream(path, FileMode.Open))
+        {
+            // Return the deserialized stream as XmlContainer
+            return serializer.Deserialize(stream) as XMLContainer;
+        }
+    }
+    // saves whatever data value is to Xml File
+    public void Save()
+    {
+        // Set data to new data
+        data = new XMLContainer();
+        // Set objects to objects in spawner
+        List<GameObject> objects = spawner.objects;
+        // Set data.spawners to new SpawnerData [objects.count]
+        data.spawners = new SpawnerData[objects.Count];
+        // For i= 0 to objects.count
+        for (int i = 0; i < objects.Count; i++)
+        
+        {
+            // Set spawnerData to new SpawnerData
+            SpawnerData spawnerData = new SpawnerData();
+            // set item to objects [i]
+            GameObject item = objects[i];
+            // Set spawner's position to position
+            spawnerData.position = transform.position;
+            // Set spawner's rotation to rotation
+            spawnerData.rotation = transform.rotation;
+            // Set data.spawners[i] = spawnerData
+            data.spawners[i] = spawnerData;
+        }
+        // Call SaveToPath(fullPath)
+        SaveToPath(fullPath);
+    }
+    // Applies the save data to scene using Spawner
+    void Apply()
+    {
+        // Set spawners to data.spawners
+        SpawnerData[] spawners = data.spawners;
+        // For i = 0 to spawners.Length
+        for (int i = 0; i < spawners.Length; i++)
+        {
+            // Set data to spawners[i]
+            SpawnerData data = spawners[i];
+            // Call spawner.Spawn() and pass data.position, data.rotation
+            spawner.Spawn(data.position, data.rotation);
+        }
+
+    }
+
+
+    void Awake()
+    {
+        // Set spawner to spawner component
+        spawner = GetComponent<Spawner>();
+    }
+    // Use this for initialization
+    void Start()
+    {
+        // Set fullPath to application.dataPath + "/" + fileName + ".xml";
+        fullPath = Application.dataPath + "/" + fileName + ".xml";
+        // If File exists at fullPath
+        if(fullPath != null)
+        {
+            // Set Data to Load(fullPath)
+            data = Load(fullPath);
+            // Call Apply
+            Apply();
+        }
+    }
+
+   
+}
